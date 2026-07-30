02b4eae31c5be9471a2fb43d939b934ed72af8b9
wohaokunr <wohaokunr@gmail.com>
2026-07-29 21:15:46 -0500

    feat(core): 添加 OpenAI 依赖包
    - 在 pyproject.toml 中添加 openai>=2.50.0 依赖
    - 更新 uv.lock 文件，添加 distro、httpcore、httpx、jiter 等相关依赖包
    - 同步更新锁文件中的依赖版本信息

---
+++ src/plugins/yawn_core/ai_call.py
@@ -0,0 +1,57 @@
+import openai
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://token-plan-cn.xiaomimimo.com/v1",
+    api_key="REDACTED",
+    # api_key="REDACTED",
+    # base_url="https://token-plan.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
+)
+
+
+
+
+
+first = client.completions.create(
+    # model="qwen3.8-max-preview",
+    model="mimo-v2.5-pro",
+    messages=[
+        {
+            "role":"user",
+            "content":"你好"
+        }
+    ]
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# first = client.responses.create(
+#     # model="qwen3.8-max-preview",
+#     model="mimo-v2.5-pro",
+#     input="记住：我喜欢 Python 和 Rust。",
+#     # store=True,
+# )
+
+# print("第一轮回答：", first.output_text)
+
+# second = client.responses.create(
+#     # model="qwen3.8-max-preview",
+#     model="mimo-v2.5-pro",
+#     previous_response_id=first.id,
+#     input="我喜欢什么？",
+#     # store=True,
+# )
+
+# print("第二轮回答：", second.output_text)
+
+#REDACTED model="qwen3.8-max-preview",
